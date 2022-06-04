9fce501c9603afe1498dc5ba40ea6c72224f4579
HariVP03 <harryskotch11@gmail.com>
2022-06-04 10:46:49 +0530

    fixed types in useContractFucntion

--- src/recoil/hooks/useContractFunction.ts
+++ src/recoil/hooks/useContractFunction.ts
@@ -1,13 +1,30 @@
-import { useContractFunction } from "@usedapp/core";
+import { Contract } from "@ethersproject/contracts";
+import { TransactionStatus, useContractFunction } from "@usedapp/core";
+import { utils } from "ethers";
+import { LogDescription } from "ethers/lib/utils";
 import { useContractInstance } from "src/hooks/web3";
 
-export const useContract = (address: string, abi: any) => {
-    const contract = useContractInstance(address, abi);
+export const useContract: (
+    address: string,
+    abi: any,
+) => [
+    Contract | undefined,
+    {
+        useContractFunc: (func: string) => {
+            send: (...args: any[]) => Promise<any>;
+            state: TransactionStatus;
+            events: LogDescription[] | undefined;
+            resetState: () => void;
+        };
+    },
+] = (address: string, abi: any) => {
+    const contractInterface = new utils.Interface(abi);
+
+    const contract = useContractInstance(address, contractInterface);
 
     const useContractFunc = (func: string) => {
-        const { state, send } = useContractFunction(contract, func);
-
-        return { state, send };
+        const t = useContractFunction(contract, func);
+        return t;
     };
 
     return [contract, { useContractFunc }];
