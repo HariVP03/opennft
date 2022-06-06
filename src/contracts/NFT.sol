1b83ba105f864fd0955fcf1fd93ef8d65283cbf4
HariVP03 <harryskotch11@gmail.com>
2022-06-06 20:44:07 +0530

    didnt ask

--- src/contracts/NFT.sol
+++ src/contracts/NFT.sol
@@ -2,14 +2,13 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
 
-contract OpenNFT is Initializable, ERC721Upgradeable, ERC721EnumerableUpgradeable, ERC721URIStorageUpgradeable, OwnableUpgradeable, UUPSUpgradeable {
+contract OpenNFT is Initializable, ERC721Upgradeable, ERC721URIStorageUpgradeable, OwnableUpgradeable, UUPSUpgradeable {
     using CountersUpgradeable for CountersUpgradeable.Counter;
 
     CountersUpgradeable.Counter private _tokenIdCounter;
@@ -21,14 +20,13 @@ contract OpenNFT is Initializable, ERC721Upgradeable, ERC721EnumerableUpgradeabl
 
     function initialize() initializer public {
         __ERC721_init("OpenNFT", "NFT");
-        __ERC721Enumerable_init();
         __ERC721URIStorage_init();
         __Ownable_init();
         __UUPSUpgradeable_init();
     }
 
     function _baseURI() internal pure override returns (string memory) {
-        return "https://hahaok.com/";
+        return "sex.com/";
     }
 
     function safeMint(address to, string memory uri) public onlyOwner {
@@ -46,13 +44,6 @@ contract OpenNFT is Initializable, ERC721Upgradeable, ERC721EnumerableUpgradeabl
 
     // The following functions are overrides required by Solidity.
 
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
-        internal
-        override(ERC721Upgradeable, ERC721EnumerableUpgradeable)
-    {
-        super._beforeTokenTransfer(from, to, tokenId);
-    }
-
     function _burn(uint256 tokenId)
         internal
         override(ERC721Upgradeable, ERC721URIStorageUpgradeable)
@@ -67,14 +58,5 @@ contract OpenNFT is Initializable, ERC721Upgradeable, ERC721EnumerableUpgradeabl
         returns (string memory)
     {
         return super.tokenURI(tokenId);
-    }
-
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721Upgradeable, ERC721EnumerableUpgradeable)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
     }
 }
